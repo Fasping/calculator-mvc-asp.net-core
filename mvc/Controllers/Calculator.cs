f56fba397a070eb8b49e0f0a8d8f7ecc673a602f
Nando <fernando.garcia@hayppgroup.com>
2023-10-12 18:45:53 +0200

    feat> clean up and create calculator file

---
+++ mvc/Controllers/Calculator.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers;
+
+public class CalculatorController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public CalculatorController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+  
+    }
+}
