87d346ccd926c6a6c20df6d950877bc050949352
Nando <fernando.garcia@hayppgroup.com>
2023-10-13 09:08:57 +0200

    Create Calculator model

---
+++ mvc/Models/Calculator.cs
@@ -0,0 +1,16 @@
+namespace mvc.Models;
+
+public class CalculatorViewModel
+{
+    public int Operator1 { get; set; }
+
+
+    public int Operator2 { get; set; }
+
+
+    public string Action { get; set; }
+
+
+    public int Answer { get; set; }
+
+}
