53d543731e64eb3d3b060e2ddfa42682e2c0ea17
Nando <fernando.garcia@hayppgroup.com>
2023-10-20 09:25:37 +0200

    fix: logic controller and manage error

--- mvc/wwwroot/js/site.js
+++ mvc/wwwroot/js/site.js
@@ -1,7 +1,7 @@
-﻿function setAction(action) {
-    document.getElementById("Action").value = action;
-}
+﻿//function setAction(action) {
+//    document.getElementById("Action").value = action;
+//}
 
-function calculate() {
-    document.forms[0].submit();
-}
+//function calculate() {
+//    document.forms[0].submit();
+//}
